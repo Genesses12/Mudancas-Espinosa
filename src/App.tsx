5eb0d8ed7f5d70fb5373463f987c3d57cfe3fba2
Genesses Fernandes <genesses12@gmail.com>
2025-10-16 16:58:08 -0300

    correção de conflito

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,23 @@
-import Index from "./pages"
+import About from "./components/About";
+import Contact from "./components/Contact";
+import Footer from "./components/Footer";
+import Header from "./components/Header";
+import Hero from "./components/Hero";
+import Services from "./components/Services";
+import Testimonials from "./components/Testimonials";
 
 function App() {
-  return (
-    <div>
-       <Index/>
-    </div>
-  )
+	return (
+		<div>
+			<Header />
+			<Hero />
+			<Services />
+			<About />
+			<Testimonials />
+			<Contact />
+			<Footer />
+		</div>
+	);
 }
 
-export default App
+export default App;
