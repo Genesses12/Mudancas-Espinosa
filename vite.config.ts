18289bafeb90ecad82a6d48adeb59989f870700e
Genesses Fernandes <genesses12@gmail.com>
2025-10-16 16:51:05 -0300

    adicionando correções

--- vite.config.ts
+++ vite.config.ts
@@ -1,14 +1,20 @@
-import tailwindcss from "@tailwindcss/vite"
-import react from "@vitejs/plugin-react"
-import path from "path"
-import { defineConfig } from "vite"
+import tailwindcss from "@tailwindcss/vite";
+import react from "@vitejs/plugin-react";
+import path from "path";
+import { defineConfig } from "vite";
 
-// https://vite.dev/config/
 export default defineConfig({
-  plugins: [react(), tailwindcss()],
-  resolve: {
-    alias: {
-      "@": path.resolve(__dirname, "./src"),
-    },
-  },
-})
+	plugins: [react(), tailwindcss()],
+	resolve: {
+		alias: {
+			"@": path.resolve(__dirname, "./src"),
+		},
+	},
+	build: {
+		outDir: "dist",
+		emptyOutDir: true,
+	},
+	server: {
+		port: 3000,
+	},
+});
